d545797bfa3054585582d26dc02315c649f2bb5f
KristinaNedzelska <78480193+KristinaNedzelska@users.noreply.github.com>
2021-02-18 11:41:57 +0200

    Update app.js

--- app.js
+++ app.js
@@ -1,14 +1,50 @@
-console.log("\nNice work! Your first application have been launched.");
-console.log("Now, I need to know your name, type it below: Kristina ");
+// Write a JavaScript function to check if given string includes given symbol.
 
-let stdin = process.openStdin();
-
-stdin.addListener("data", (txt) => {
-    if(txt.toString().trim() === "quit") {
-        console.log("\nHave a nice JS trip! Bye-bye");
-        stdin.end();
+function checkStringContainsSymbol(string, symbol) {
+    if (string.includes(symbol)) {
+        console.log("True");
     } else {
-        console.log(`\nHi ${txt.toString().trim()}, nice to meet you at Eleks QA Academy 2021. I am sure you'll become great in your job.`);
-        console.log(`${txt.toString().trim()}, to quit this application you can use 'Ctrl + C' keys, or type 'quit' and hit 'Enter'`); 
-    }   
-  });
+        console.log("False");
+    }
+}
+checkStringContainsSymbol("Hello world", "w")
+
+// Write a JavaScript function to check whether a string is blank or not (string with spaces should be also blank).
+
+function isStringBlank(string) {
+    if (string.length === 0 || string.includes(' ')) {
+        console.log('Your string is blank');
+    } else {
+        console.log('String is NOT blank')
+    }    
+}
+
+isStringBlank("Hello world");
+
+
+// Write a JavaScript function to convert a string in abbreviated form. console.log(abbrev (“Nazar Babii")) – should be “N.B.” (should convert lower case names to upper)
+
+function abbrev(name) {
+    let firstSymbol = name.charAt(0);
+    let spaceSymbol = name.indexOf(" ");
+    let positionSecondSymbol = spaceSymbol+1;
+    let secondSymbol = name.charAt(positionSecondSymbol);
+    return `${firstSymbol}.${secondSymbol}.`;
+}
+
+let result = abbrev("Kristina Nedzelska");
+console.log(result);
+
+// Write a JavaScript function that accept two integers and display the larger.
+
+function maxNumber(x, y) {
+    if (x > y) {
+        console.log(x);
+    } else {
+        console.log(y);
+    }
+}
+maxNumber(2,4)
+
+// Write a JavaScript function with conditional statement to sort three numbers. (use if/else only)
+

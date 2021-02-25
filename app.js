c07294de8f9de3bc0ce66ad3270cad419b08af62
KristinaNedzelska <78480193+KristinaNedzelska@users.noreply.github.com>
2021-02-25 11:14:52 +0200

    1-4 tasks

--- app.js
+++ app.js
@@ -1,14 +1,71 @@
-console.log("\nNice work! Your first application have been launched.");
-console.log("Now, I need to know your name, type it below: Kristina ");
+class Vehicle {
+    constructor(carModel, carYear, maxSpeed) {
+        this.model = carModel;
+        this.year = carYear;
+        this.maxSpeed = maxSpeed;
+    }
 
-let stdin = process.openStdin();
+    displayInfo() {
+        console.log(`name: ${this.model};
+        year: ${this.year};
+        Max Speed: ${this.maxSpeed};
+        Type: ${this.type}:`);
+    }
+}
 
-stdin.addListener("data", (txt) => {
-    if(txt.toString().trim() === "quit") {
-        console.log("\nHave a nice JS trip! Bye-bye");
-        stdin.end();
-    } else {
-        console.log(`\nHi ${txt.toString().trim()}, nice to meet you at Eleks QA Academy 2021. I am sure you'll become great in your job.`);
-        console.log(`${txt.toString().trim()}, to quit this application you can use 'Ctrl + C' keys, or type 'quit' and hit 'Enter'`); 
-    }   
-  });
+class Car extends Vehicle {
+    constructor(carModel, carYear, maxSpeed) {
+        super(carModel, carYear, maxSpeed);
+        this.type = "car";
+    }
+
+    transportPeople() {
+        console.log(`I am starting transporting passendger`);
+    }
+    
+}
+
+class Truck extends Vehicle {
+    constructor(carModel,carYear, maxSpeed) {
+        super(carModel, carYear, maxSpeed);
+        this.type = "truc";
+    }
+
+    transportContainer() {
+        console.log(`I am starting transportig heavy container`);
+    }
+    
+    static staticTransport() {
+        console.log(`I am static method`);
+    }
+}
+
+class Bus extends Vehicle {
+    constructor(carModel,carYear, maxSpeed){
+        super(carModel, carYear, maxSpeed);
+        this.type = "bus";
+       }
+    displayInfo() {
+        console.log(`model: ${this.model}`);
+        }
+}
+
+let vehicle = new Vehicle("test", 1990, 120);
+vehicle.displayInfo();
+
+let car = new Car("testCar",'',250);
+console.log(car);
+car.transportPeople();
+//Call method of father’s class
+car.displayInfo(); 
+
+let truck = new Truck("Man", 2000, 120);
+truck.displayInfo();
+truck.transportContainer();
+
+//Add static method to one class, try to call it and investigate output.
+Truck.staticTransport();
+
+//Override method of father’s class.
+let bus = new Bus("Volvo",1997,90)
+bus.displayInfo();

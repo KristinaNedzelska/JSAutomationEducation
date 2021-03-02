2f5ad9b9bbbaccc3d1d8894e75388ff47456191a
KristinaNedzelska <78480193+KristinaNedzelska@users.noreply.github.com>
2021-03-02 10:55:33 +0200

    Finally I did it

--- app.js
+++ app.js
@@ -1,14 +1,34 @@
-console.log("\nNice work! Your first application have been launched.");
-console.log("Now, I need to know your name, type it below: Kristina ");
+//Write a JavaScript function to get the last element of an array
+let salat = ["Tomato", "Cucamber", "Peper"];
+let newSalat = function(ingredients) { 
+console.log(`Last element of array is: ${ingredients[ingredients.length -1]}`);
+}
+newSalat(salat);
 
-let stdin = process.openStdin();
+//Write a simple JavaScript program to join all elements of the following array into a string
+let flowers = ["Tulip", "Rose", "Peony", "Iris"];
+console.log(`String of array elements: ${flowers.join(" ")}`);
 
-stdin.addListener("data", (txt) => {
-    if(txt.toString().trim() === "quit") {
-        console.log("\nHave a nice JS trip! Bye-bye");
-        stdin.end();
-    } else {
-        console.log(`\nHi ${txt.toString().trim()}, nice to meet you at Eleks QA Academy 2021. I am sure you'll become great in your job.`);
-        console.log(`${txt.toString().trim()}, to quit this application you can use 'Ctrl + C' keys, or type 'quit' and hit 'Enter'`); 
-    }   
-  });
+//There are two arrays with individual values, write a JavaScript program to compute the sum of each individual index value from the given arrays and save them to third array
+let firstArray = [1,6,8,5];
+let secondArray = [2,4,7,0];
+let thirdArray = [];
+for (let i=0; i <= firstArray.length -1; i++) {
+thirdArray.push(firstArray[i]+secondArray[i]);
+}
+thirdArray;
+
+//Write a JavaScript program to calculate the area and perimeter of a rectangle. 
+//Rectangle should be an object with properties width and height and methods getArea() and getPerimeter();
+let rectangle = {
+width: 10,
+height: 5,
+getArea() {
+console.log(this.width * this.height);
+},
+getPerimeter() {
+console.log(2*(this.width + this.height));
+}
+}
+rectangle.getArea();
+rectangle.getPerimeter();
